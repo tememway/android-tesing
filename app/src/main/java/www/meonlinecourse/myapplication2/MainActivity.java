5a52a93595f6daf266a4eedba834b5236d51e049
tememway <tamem20142016@gmail.com>
2020-12-13 17:48:54 +0800

    first commit

---
+++ app/src/main/java/www/meonlinecourse/myapplication2/MainActivity.java
@@ -0,0 +1,17 @@
+package www.meonlinecourse.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int ar[]=new int[5] ;
+        for(int i=0;i<5;i++);
+    }
+}
